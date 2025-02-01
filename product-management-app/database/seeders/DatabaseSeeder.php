d530bbf3b417174a4c5ad53f6c993e6b4200e37e
Estefanyq19 <estefanyq1908@gmail.com>
2025-02-01 12:21:09 -0600

    creacion de seeder

--- product-management-app/database/seeders/DatabaseSeeder.php
+++ product-management-app/database/seeders/DatabaseSeeder.php
@@ -11,13 +11,13 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
-        // User::factory(10)->create();
+    
+     public function run(): void
+     {
+        // Llama a UserSeeder
+        $this->call(UserSeeder::class);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // Llama a ProductSeeder
+        $this->call(ProductSeeder::class);
     }
 }
